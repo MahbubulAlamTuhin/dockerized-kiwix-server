6346729ea8b187e808ba7962cd0458cb34718e00
Ipek Baris <deneme@Niemands-MacBook-Pro.local>
2020-11-30 13:34:57 +0100

    add updated link

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,6 +2,6 @@
 
 # Install kiwix-tools
 # wget https://ftp.fau.de/kiwix/nightly/2017-08-13/kiwix-tools_linux64_2017-08-13.tar.gz
-wget https://ftp.fau.de/kiwix/nightly/2018-06-01/kiwix-tools_linux-x86_64-2018-06-01.tar.gz
+wget https://ftp.fau.de/kiwix/nightly/2020-10-30/kiwix-tools_linux-x86_64-2020-10-30.tar.gz
 # tar -xvzf kiwix-tools_linux-x86_64-2018-06-01.tar.gz -C ./bin
-tar -xvzf kiwix-tools_linux-x86_64-2018-06-01.tar.gz -C ./bin --strip-components 1
+tar -xvzf kiwix-tools_linux-x86_64-2020-10-30.tar.gz -C ./bin --strip-components 1
